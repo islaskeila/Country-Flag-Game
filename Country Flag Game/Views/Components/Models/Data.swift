03dea3613c76b712546040c01ffd5ec3b9d65f85
Keila Islas <kislas@bsd220.org>
2024-05-17 09:26:15 -0500

    Data and pictures added

--- Country Flag Game/Views/Components/Models/Data.swift
+++ Country Flag Game/Views/Components/Models/Data.swift
@@ -6,3 +6,34 @@
 //
 
 import Foundation
+
+struct Data {
+    let countries = [
+    "Angola",
+    "Austria",
+    "China",
+    "England",
+    "Estonia",
+    "France",
+    "Germany",
+    "Greece",
+    "India",
+    "Indonesia",
+    "Ireland",
+    "Israel",
+    "Italy",
+    "Madagascar",
+    "Mexico",
+    "Norway",
+    "Pakistan",
+    "Peru",
+    "Saudi Arabia",
+    "South Africa",
+    "Spain",
+    "Sweden",
+    "The Bahamas",
+    "Ukraine",
+    "United States",
+    "Vietnam"
+    ]
+}
