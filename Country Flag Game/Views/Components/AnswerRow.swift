ad1839abc8d45a565a73455e5e5f8d2d053e5a81
Keila Islas <kislas@bsd220.org>
2024-05-16 23:34:30 -0500

    Answer row finished(with minor bugs)

--- Country Flag Game/Views/Components/AnswerRow.swift
+++ Country Flag Game/Views/Components/AnswerRow.swift
@@ -8,11 +8,34 @@
 import SwiftUI
 
 struct AnswerRow: View {
+    @EnvironmentObject var gameManager: GameManger
+    var answer: Answer
+    @State private var isSelected = false
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        HStack(spacing: 20, content: {
+            Image(systemName: "circle.fill")
+                .font(.caption)
+            Text(answer.text)
+                .font(.title)
+            if isSelected {
+                Spacer()
+                Image(systemName: answer.isCorrect ? .green : .red)
+            }
+        })
+        .padding()
+        .frame(width: 300, alignment: .leading)
+        .background(.white)
+        .cornerRadius(10)
+        .shadow(color: isSelected ? (answer.isCorrect ? .green : .red) : .gray, radius: 5, x: 0.5, y: 0.5)
+        onTapGesture {
+            if !gameManager.answerSelected {
+                isSelected = true
+                gameManager.selectAnswer(answer: answer)
+            }
+        }
     }
 }
 
 #Preview {
-    AnswerRow()
+    AnswerRow(answer: Answer(text: "Test", isCorrect: tue))
 }
